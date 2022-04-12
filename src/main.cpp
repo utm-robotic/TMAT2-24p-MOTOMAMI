f8e0097d90d0547748459142b0b8ab6dc6bc72b5
Deyla Hernández Mata <deylahdzm11@gmail.com>
2022-04-12 12:16:40 -0500

    motomami jala pero le falta el ciclo de tippings

--- src/main.cpp
+++ src/main.cpp
@@ -28,60 +28,44 @@ void pre_auton(void) {
 
 
 void autonomous(void) {
-  avanzar(1, 28, 100);
+  avanzar(1, 20, 100);
   detener1();
   PD.set(1);
   task::sleep(200);
-  bd(150);
+  bd(230);
   task::sleep(200);
-  avanzar(2, 17, 100);
+  avanzar(2, 14, 100);
   task::sleep(200);
-  girar(-90,20);
-  task::sleep(200);
-  JH.set(1);
-  /*avanzar(1, 5, 60);
-  task::sleep(200);
-  bd(-120);
-  task::sleep(200);
-  PD.set(0);
+  girar(-90,10);
   task::sleep(200);
   JH.set(1);
-  task::sleep(200);*/
+  task::sleep(200);
   pararBumper1();
   task::sleep(200);
   JH.set(0);
   task::sleep(200);
   avanzar(1, 10, 100);
   task::sleep(200);
-  girar(-90, 30);
-  task::sleep(200);
+  /*girar(-90, 30);
+  task::sleep(200);*/
   bandaveloz(80);
   banda(1);
   task::sleep(2000);
   banda(3);
-  cicloT(9);
   task::sleep(200);
-  girar(-90, 30);
+  avanzar(2, 5, 80);
+  task::sleep(200);
+  girar(-180,10);
+  avanzar(1, 8, 80);
+
+  /*cicloT(9);
+  task::sleep(200);
+  girar(-90, 30);*/
 
 
   
   
-  /*girar(90, 30);
-  task::sleep(200);
-  Bum.pressed(pararBumper);
-  avanzarhasta(2, 70, 6);
-  task::sleep(200);
-  JH.set(0);
-  task::sleep(200);*/
-  /*bandaveloz(80);
-  banda(1);
-  task::sleep(2000);
-  banda(3);
-  cicloT(9);
-  girar(-180, 3);
-  avanzar(2, 8, 20);
-  JH.set(1);
-  avanzar(1, 6, 50);*/
+  
  
 }
 
@@ -91,12 +75,12 @@ void usercontrol(void) {
   resetea();
   parar();
   velocidad(100,100);
-  int x=1;
+  int x=4;
   int y=1;
   
   while (true) {
-    LeftDriveSmart.spin(forward,Controller1.Axis3.value(),percent); 
-    RightDriveSmart.spin(forward,Controller1.Axis2.value(),percent);
+    RightDriveSmart.spin(reverse, (Controller1.Axis1.value() - Controller1.Axis3.value()), percent);
+    LeftDriveSmart.spin(forward, (Controller1.Axis1.value() + Controller1.Axis3.value()), percent);
 
     if(Controller1.ButtonR1.pressing()){
       BD.spin(forward);
@@ -118,7 +102,7 @@ void usercontrol(void) {
       BI.stop(hold);
     }
 
-   if(Controller1.ButtonUp.pressing() && x==1){
+   if(Controller1.ButtonUp.pressing() && x==4){
       PI.set(1);
       task::sleep(200);
       x=2;
@@ -126,7 +110,7 @@ void usercontrol(void) {
   else if (Controller1.ButtonUp.pressing() && x==2) {
       PI.set(0);
       task::sleep(200);
-      x=1;
+      x=4;
     }
 
    
@@ -155,19 +139,30 @@ void usercontrol(void) {
       x=1;
     }
 
-    if(Controller1.ButtonB.pressing() && x==1){
-      JH.set(0);
+    if(Controller1.ButtonB.pressing()){
+      x = 4;
       task::sleep(200);
-      x=2;
     }
-    else if (Controller1.ButtonB.pressing() && x==2) {
+
+    
+
+    // bumper
+
+  if(Bum.pressing()){
+    x = 5;
+  }
+  if(x== 4 ){
+    JH.set(1);
+  }
+
+  if(x == 5){
+    JH.set(0);
+    if(Controller1.ButtonB.pressing()){
       JH.set(1);
-      task::sleep(200);
-      x=1;
-    }
-    
-    
-    wait(20, msec); 
+    }else{JH.set(0);}
+  }
+
+  wait(20, msec); 
   }
 }
 
